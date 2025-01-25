fe4dd040984dc9526cd35c9a0a447c5cc23d4ad6
cldrake01 <collinlindendrake@gmail.com>
2025-01-25 15:28:02 -0700

    Even more work on the API routes. Reworked the Menubar as well.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,5 @@
 	import MapSelector from "$lib/components/maps/MapSelector.svelte";
 </script>
 
-<MapSelector />
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
