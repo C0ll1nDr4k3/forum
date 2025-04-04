b7f41ccadb570b9175ff34d5bc3a1d5ed31744b6
cldrake01 <collinlindendrake@gmail.com>
2025-04-04 14:07:37 -0600

    //

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,40 +1,40 @@
-import type { LayoutServerLoad } from "./$types";
-
-export const load: LayoutServerLoad = async (event) => {
-	// Define the list of authentication-related routes
-	// const authPages = [
-	// 	"/login",
-	// 	"/signin",
-	// 	"/auth/signin",
-	// 	"/signup",
-	// 	"/forgot-password",
-	// 	"/reset-password"
-	// 	// Add any other auth routes here
-	// ];
-	//
-	// const currentPath = event.url.pathname;
-	//
-	// // Check if the current path is an authentication page
-	// const isAuthPage = authPages.some((path) => currentPath === path);
-	//
-	// if (!isAuthPage) {
-	// 	// Perform the session check only if not on an auth page
-	// 	const session = await event.locals.auth();
-	//
-	// 	if (!session?.user?.id) {
-	// 		// Use 'throw redirect' to perform the redirect
-	// 		throw redirect(303, `/auth/signin`);
-	// 	}
-	//
-	// 	return {
-	// 		session
-	// 	};
-	// }
-	//
-	// If on an auth page, optionally fetch session or other data as needed
-	const session = await event.locals.auth();
-
-	return {
-		session
-	};
-};
+// import type { LayoutServerLoad } from "./$types";
+//
+// export const load: LayoutServerLoad = async (event) => {
+// 	// Define the list of authentication-related routes
+// 	// const authPages = [
+// 	// 	"/login",
+// 	// 	"/signin",
+// 	// 	"/auth/signin",
+// 	// 	"/signup",
+// 	// 	"/forgot-password",
+// 	// 	"/reset-password"
+// 	// 	// Add any other auth routes here
+// 	// ];
+// 	//
+// 	// const currentPath = event.url.pathname;
+// 	//
+// 	// // Check if the current path is an authentication page
+// 	// const isAuthPage = authPages.some((path) => currentPath === path);
+// 	//
+// 	// if (!isAuthPage) {
+// 	// 	// Perform the session check only if not on an auth page
+// 	// 	const session = await event.locals.auth();
+// 	//
+// 	// 	if (!session?.user?.id) {
+// 	// 		// Use 'throw redirect' to perform the redirect
+// 	// 		throw redirect(303, `/auth/signin`);
+// 	// 	}
+// 	//
+// 	// 	return {
+// 	// 		session
+// 	// 	};
+// 	// }
+// 	//
+// 	// If on an auth page, optionally fetch session or other data as needed
+// 	const session = await event.locals.auth();
+//
+// 	return {
+// 		session
+// 	};
+// };
